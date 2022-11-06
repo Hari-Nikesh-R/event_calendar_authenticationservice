1c168bea5d0a3cc94f40949bb227cbab3df87af2
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2022-11-06 17:51:51 +0530

    AuthController initiated.

---
+++ authenticationService/src/main/java/com/example/authenticationService/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.example.authenticationService.controller;
+
+import com.example.authenticationService.model.AuthDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "login/")
+public class AuthController {
+    @PostMapping
+    public void loginUser(AuthDto authDto)
+    {
+
+    }
+
+}
