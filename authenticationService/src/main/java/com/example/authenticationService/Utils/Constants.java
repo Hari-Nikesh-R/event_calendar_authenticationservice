afac01a7fd44cc0aec54c97d7849bd5097bce15b
indhumathi120 <indhumathi.c.cce@sece.ac.in>
2022-11-28 00:04:17 +0530

    Utility->Validation->Password,EmailId logic written

--- authenticationService/src/main/java/com/example/authenticationService/Utils/Constants.java
+++ authenticationService/src/main/java/com/example/authenticationService/Utils/Constants.java
@@ -19,7 +19,8 @@ public interface Constants {
     String SUB = "sub";
 
     String OR = " or ";
-
+    String EMAIL_VALIDATION="^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
 
 
--- authenticationService/src/main/java/com/example/authenticationService/Utils/Utility.java
+++ authenticationService/src/main/java/com/example/authenticationService/Utils/Utility.java
@@ -3,11 +3,17 @@ package com.example.authenticationService.Utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.example.authenticationService.Utils.Constants.EMAIL_VALIDATION;
 import static com.example.authenticationService.Utils.Constants.PASSWORD_VALIDATION;
 
 public class Utility {  public static boolean validatePassword(String password){
     Pattern pattern= Pattern.compile(PASSWORD_VALIDATION);
     Matcher matcher=pattern.matcher(password);
     return matcher.matches();
+}
+public static boolean validateEmailId(String mailId){
+    Pattern pattern=Pattern.compile(EMAIL_VALIDATION);
+    Matcher matcher=pattern.matcher(mailId);
+    return  matcher.matches();
 }
 }
